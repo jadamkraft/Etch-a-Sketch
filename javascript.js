f3a4506888f1a80db4ee65a552e960f6f16e0b60
jadamkraft <jadamkraft@gmail.com>
2022-06-16 16:25:49 -0500

    Add box with javascript

--- javascript.js
+++ javascript.js
@@ -1,10 +1,22 @@
 /* javascript.js */
 
-document.addEventListener("load", addElement);
 
-function addElement() {
-    const contentDiv = document.createElement("div");
-    const rowOne = contentDiv.className = "rowOne";
-    contentDiv.appendChild(rowOne);
-    div.container.appendChild(contentDiv);
-}
+const containerDiv = document.querySelector('.container');
+
+window.addEventListener('load', (e) => {
+     let divOne = document.createElement('div');
+     let divTwo = document.createElement('div')
+     let divThree = document.createElement('div');
+     let divFour = document.createElement('div');
+     let divBox = document.createElement('div');
+     divOne.className = 'row-one';
+     divTwo.className = 'row-two';
+     divThree.className = 'row-three';
+     divFour.className = 'row-four';
+     divBox.className = 'box';
+     containerDiv.appendChild(divOne);
+     containerDiv.appendChild(divTwo);
+     containerDiv.appendChild(divThree);
+     containerDiv.appendChild(divFour);
+     divOne.appendChild(divBox);
+});
