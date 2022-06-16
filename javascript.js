916f5e7eec06955cea697f334fe4725f7ba6f147
jadamkraft <jadamkraft@gmail.com>
2022-06-16 15:57:39 -0500

    Get javascript to insert a div on page load

--- javascript.js
+++ javascript.js
@@ -1,2 +1,10 @@
 /* javascript.js */
 
+document.addEventListener("load", addElement);
+
+function addElement() {
+    const contentDiv = document.createElement("div");
+    const rowOne = contentDiv.className = "rowOne";
+    contentDiv.appendChild(rowOne);
+    div.container.appendChild(contentDiv);
+}
